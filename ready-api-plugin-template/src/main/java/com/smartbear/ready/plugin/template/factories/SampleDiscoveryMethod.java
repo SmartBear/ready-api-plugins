51e8ae419608c397486893f81f0f8dce52625f25
VinhTran <vinh.tran@smartbear.com>
2022-12-01 15:49:04 +0700

    RIA-19786 fix vulnerabilities for readyapi plugin

--- ready-api-plugin-template/src/main/java/com/smartbear/ready/plugin/template/factories/SampleDiscoveryMethod.java
+++ ready-api-plugin-template/src/main/java/com/smartbear/ready/plugin/template/factories/SampleDiscoveryMethod.java
@@ -2,6 +2,7 @@ package com.smartbear.ready.plugin.template.factories;
 
 import com.eviware.soapui.impl.actions.DiscoveryMethod;
 import com.eviware.soapui.impl.rest.discovery.DiscoveredRequest;
+import com.eviware.soapui.impl.rest.discovery.RestDiscoveryMethod;
 import com.eviware.soapui.model.workspace.Workspace;
 import com.eviware.soapui.plugins.auto.PluginDiscoveryMethod;
 import com.eviware.soapui.support.UISupport;
@@ -28,6 +29,11 @@ public class SampleDiscoveryMethod implements DiscoveryMethod {
     @Override
     public String getDescription() {
         return "Dummy Discovery method - doesn't actually do anything.";
+    }
+
+    @Override
+    public RestDiscoveryMethod getRestDiscoveryMethod() {
+        return null;
     }
 
     @Override
